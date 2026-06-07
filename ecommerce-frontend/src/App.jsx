912005bb1109eacd462df61e9ce848de40650fb5
Najia <anajia231@gmail.com>
2026-06-07 22:13:17 +0430

    add main application component with routing and cart functionality

---
+++ ecommerce-frontend/src/App.jsx
@@ -0,0 +1,33 @@
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import { Routes, Route} from 'react-router';
+import { HomePage } from './pages/home/HomePage';
+import { Checkout } from './pages/CheckoutPage/CheckoutPage';
+import { OrdersPage } from './pages/orders/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
+
+function App() {
+  const [cart, setCart] = useState([]);
+
+  const loadCart = async () => {
+    const response = await axios.get('/api/cart-items?expand=product');
+    setCart(response.data);
+  }
+  
+  useEffect(() => {
+    loadCart();
+  }, [])
+
+  return (
+    <>
+      <Routes>
+        <Route index element={<HomePage cart={cart} loadCart={loadCart}/>} />
+        <Route path='checkout' element={<Checkout cart={cart} loadCart={loadCart} />} />
+        <Route path='orders' element={<OrdersPage cart={cart}/>} />
+        <Route path='tracking' element={<TrackingPage/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App
